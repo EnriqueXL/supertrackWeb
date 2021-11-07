d31664ca347a56433c9359dcb672fbf6be45f13a
Luis-Enrique <luiswetowkn@gmail.com>
2021-11-07 12:56:40 -0600

    original

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const ConseguirLugares = () => {
     fetch('https://crsseguridad.live/intranet/encustascrs/prueba-cleaver/consultweb.php')
         .then(response => response.json())
         .then(lugares => {
-            console.log(lugares)
 
             lugares.forEach(lugar => {
                 let lugarInfo = {
@@ -11,27 +10,32 @@ const ConseguirLugares = () => {
                     nombre: lugar.Nombre
                 }
                 lugaresInfo.push(lugarInfo)
-            });
+            })
+
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(usuarioUbicacion => {
                     let ubicacion = {
                         lat: usuarioUbicacion.coords.latitude,
                         lng: usuarioUbicacion.coords.longitude
                     }
+                    console.log("arreglo de mi ubicacion", ubicacion);
                     dibujarMapa(ubicacion)
                 })
             }
         })
 }
 const dibujarMapa = (obj) => {
+    console.log("objetComo recibe el objeto", obj);
     let mapa = new google.maps.Map(document.getElementById('map'), {
-        center: obj,
-        zoom: 4
+        zoom: 15,
+        center: obj
     })
+
     let marcadorUsuario = new google.maps.Marker({
         position: obj,
         title: 'Tu ubicacion'
     })
+
     marcadorUsuario.setMap(mapa)
     let marcadores = lugaresInfo.map(lugar => {
         return new google.maps.Marker({
